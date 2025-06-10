1cf310842a87bb7e172eacb3f27c7fb5acb96782
Andres <camolano85@gmail.com>
2025-06-09 19:11:05 -0500

    09052025-v1

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,31 @@
-alert("Hola, Equipo desde archivo externo")
+/*alert("Hola, Equipo desde archivo externo")*/
+
+/*console.log(". Enviando mensaje a la consola")*/
+
+/*VARIABLES*/
+/*
+nombre1 = 1
+var nombre2 = 2
+let nombre3 = 3
+const nombre4 = 4
+
+console.log(nombre1);
+console.log(nombre2);
+console.log(nombre3);
+console.log(nombre4);
+*/
+
+/*Ambito de las variables*/
+
+/*const nombre = "pipita";
+
+
+{
+   console.log(nombre); 
+   sobrenombre = "perez"
+   
+}
+console.log(sobrenombre);*/
+
+
+

be8cc10dc761fa70320fae3facfa37ece751d51c
Andres <camolano85@gmail.com>
2025-06-12 18:26:56 -0500

    120625-1826

--- javascript/basicos.js
+++ javascript/basicos.js
@@ -57,9 +57,72 @@ console.log(!false)
 */
 
 /* Operadores de concatenacion */
-
+/*
 console.log('Andres' + ' ' + 'Molano');
 
 const numero1 = 10;
 const numero2 = '10';
 console.log(numero1 + Number(numero2));
+*/
+
+/* secuencia */
+/*
+console.log('hola');
+console.log('que mas?');
+console.log('chaito');
+*/
+
+/* Control de flujo */
+/*
+console.log('Buenos dias');
+console.log('Buenas tardes');
+console.log('Buenas noches');
+*/
+
+/*Estructura de control*/
+
+     /* Condicionales*/
+/*
+     if(8 < 4){
+        console.log('Si, se cumplio la condicion');
+     }
+     else{
+        console.log('No, se cumplio la condicion');
+     }
+
+
+const hora = 19;     
+if(hora > 0 && hora <= 12){
+console.log('Buenos dias');
+}
+else if(hora >= 12 && hora <= 18){
+    console.log('Buenas tardes');
+}
+else{console.log('Buenas noches')};
+*/
+/*
+const perfil = 'cliente'
+
+switch (perfil){
+    case 'administrador':
+console.log('Consola de administracion');
+    break;
+
+    case 'cliente':
+console.log('tienda virtual');    
+    break;
+
+    case 'contabilidad':
+console.log('Dirigir a finanzas');    
+    break;
+
+    default:
+console.log('Acceso no autorizado');
+    break;
+    
+}
+
+
+
+     console.log('continua la secuencia');
+*/
---
+++ javascript/ejercicios.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/* Preguntar nombre de usuario y saludarlo por su nombre*/
+/*
+let nombre = prompt('Cual es tu nombre');
+alert('Hola'+ ' '+ nombre);
+*/
